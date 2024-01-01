f835b64b50dd984aa128df7b390d1f7aadcc4c16
kanikarawal <151913973+kanikarawal@users.noreply.github.com>
2024-01-01 22:44:48 +0530

    Delete Front-End directory

--- Front-End/src/services/TokenUtil.js
+++
@@ -1,9 +0,0 @@
-export function isAuthenticated(){
-    return getToken() ? true:false;
-}
-export function getToken(){
-    return localStorage.getItem("token");
-}
-export function logout(){
-    localStorage.removeItem("token");
-}
